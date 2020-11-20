32e8876c9e5495be01b277a91f0052d2f13e63d5
LouangeMu <louangemuhawe2020@gmail.com>
2020-11-20 16:46:53 +0200

    This is the configuration of a sequelize

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+import {config} from 'dotenv';
+config();
 
 export const development = {
   url: process.env.DATABASE_URL,
@@ -7,13 +8,13 @@ export const development = {
 };
 
 export const test = {
-  url: process.env.DATABASE_URL,
+  url: process.env.TEST_DATABASE_URL,
   dialect: 'postgres',
   logging: false,
 };
 
 export const production = {
-  url: process.env.DATABASE_URL,
+  url: process.env.TEST_DATABASE_URL,
   dialect: 'postgres',
   logging: false,
 };
