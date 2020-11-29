823ad72e7d2b4e25bc194c511638cd6d5e88609a
tigthor <tigthor1337@gmail.com>
2020-11-29 15:04:28 +0200

    ft: email sent on success user creation - User is created - user is sent a verification email
    [start #175494862]
    [finsih #175494862]

--- src/controllers/Index.js
+++ src/controllers/Index.js
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import models from "../database/models";
 import EncryptPassword from "../helpers/passEncrypt";
@@ -80,3 +81,82 @@ class UserController {
 
 export default UserController;
 >>>>>>> 9dfd941 (adding verification)
+=======
+import models from '../database/models';
+import EncryptPassword from '../helpers/passEncrypt';
+import tokenizer from '../helpers/tokenizer';
+import accountVerify from '../helpers/acountVerify';
+import sendMail from '../helpers/mailSender';
+
+class UserController {
+  static async signup(req, res) {
+    try {
+      const {
+        fullname,
+        email,
+        birthdate,
+        gender,
+        tel,
+        city,
+        country,
+      } = req.body;
+
+      const password = EncryptPassword(req.body.password);
+
+      const newUser = {
+        fullname,
+        email,
+        password,
+        birthdate,
+        gender,
+        tel,
+        country,
+        city,
+        role: 'REQUESTER',
+        // token,
+        isVerified: false,
+      };
+      const createUser = await models.User.create(newUser);
+
+      res.status(201).send({ message: 'success', createUser });
+      const tokenToSend = tokenizer.generate(email);
+      sendMail(fullname, email, tokenToSend);
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async emailVerify(req, res) {
+    const { token } = req.query;
+    const stringfyToken = token.String()
+    try {
+      const { email } = await tokenizer.verify(string);
+      const findReplaceByEmail = await models.User.update(
+        { isVerified: true },
+        {
+          where: { email },
+        },
+      );
+      if (findReplaceById) {
+        res.status(200).json({
+          status: 200,
+          message: 'Verification Successfuly Verified',
+        });
+      } else {
+        res.status(403).json({
+          status: 403,
+          message: 'Invalid verification link',
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        status: 500,
+        message: 'Error Occured',
+        Error_occured: error,
+      });
+    }
+  }
+}
+
+export default UserController;
+>>>>>>> 3baadcc (ft: email sent on success user creation)
--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import { Op } from 'sequelize';
 import db from '../database/models';
@@ -28,3 +29,32 @@ class Queries {
 
 export default Queries;
 >>>>>>> 3f5e3cc (work in progress)
+=======
+import { Op } from "sequelize"
+import db from "../database/models"
+
+class Queries {
+  static async create(table, data) {
+    try {
+      const datas = await table.create(data)
+      return datas
+    } catch (error) {
+      return error
+    }
+  }
+
+  static async findOrCreate(table, data, condition) {
+    try {
+      const datas = await table.findOrCreate({
+        where: condition,
+        defaults: data,
+      })
+      return datas[0]
+    } catch (error) {
+      return error
+    }
+  }
+}
+
+export default Queries
+>>>>>>> 3baadcc (ft: email sent on success user creation)
